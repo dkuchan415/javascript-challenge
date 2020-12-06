419fe70bd3610ec21fe2c2838877970fd0148316
dkuchan415 <danielkuchan@yahoo.com>
2020-12-06 16:16:37 -0600

    Completed filtering of date

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,61 +6,74 @@ var tableData = data;
 
 // Log the UFO sighting data from the other file
 
-console.log(tableData)
-
-// tableData.forEach(function(ufoSighting) {
-//     console.log(ufoSighting);
-// })
-
-// Append rows from js table into html table
-
-// Create variable locating table body in d3
+// console.log(tableData)
 
 var tbody = d3.select("tbody")
 
-// Append table rows for each UFO sighting using d3
-
-// data.forEach(function(ufoSighting) {
-//     console.log(ufoSighting);
-//     var row = tbody.append("tr");
-// });
-
-// // Use object entries to log each UFO sighting
-
-// data.forEach(function(ufoSighting) {
-//     console.log(ufoSighting);
-//     var row = tbody.append("tr");
-
-//     Object.entries(ufoSighting).forEach(function([key, value]) {
-//         console.log(key, value);
-//     });
-// });
-
-// // Use d3 to append  cell per UFO sighting
-// data.forEach(function(ufoSighting) {
-//     console.log(ufoSighting);
-//     var row = tbody.append("tr");
-
-//     Object.entries(ufoSighting).forEach(function([key, value]) {
-//         console.log(key, value);
-//         // Append a cell to the row for each value
-//         // in the weather report object
-//         var cell = row.append("td");
-//     });
-// });
 
 // Use d3 to update each cell's text with UFO sighting values 
 // (date, city, etc.)
 
 data.forEach(function(ufoSighting) {
-    console.log(ufoSighting);
+    // console.log(ufoSighting);
     var row = tbody.append("tr");
 
     Object.entries(ufoSighting).forEach(function([key, value]) {
-        console.log(key, value);
+        // console.log(key, value);
         // Append a cell to the row for each value
         // in the weather report object
         var cell = row.append("td");
         cell.text(value)
     });
 });
+
+// Filter table based on date
+
+var button = d3.select('#filter-btn');
+
+// create event handlers
+//add form later to allow 'enter' to filter
+
+button.on("click", runButton);
+
+// Create event handler function
+
+function runButton() {
+    //Prevent page from refreshing
+    d3.event.preventDefault();
+
+    //select input element and get raw HTML node
+    var inputElement = d3.select("#datetime").property("value");
+
+    console.log(inputElement);
+
+    //If statement for blank input
+
+    if (inputElement) {
+        var filteredData = tableData.filter((ufo) => ufo.datetime === inputElement);
+    }
+    else {
+        var filteredData = tableData;
+    }
+
+    console.log(filteredData);
+
+    // remove all dates from table
+    tbody.html("");
+
+    // Build a table with the new filtered data
+
+    filteredData.forEach(function(ufoSighting) {
+        console.log(ufoSighting);
+        var row = tbody.append("tr");
+    
+        Object.entries(ufoSighting).forEach(function([key, value]) {
+            console.log(key, value);
+            // Append a cell to the row for each value
+            // in the weather report object
+            var cell = row.append("td");
+            cell.text(value)
+        });
+    });
+};
+
